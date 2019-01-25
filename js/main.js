55976a6e1e57428b6c894f8d380efc656b58e464
keemdb <keemdb@gmail.com>
2019-01-26 02:33:14 +0900

    반응형 작업중

--- js/main.js
+++ js/main.js
@@ -1,20 +1,6 @@
 const log = console.log;
 
-$(window).scroll(function () { 
-	var scTop = $(window).scrollTop();
-	if( 0 < scTop) {
-		$("header").stop().animate({
-			"max-width":"100%",
-			"top":0,
-		}, 200)
-	}
-	if( 0 == scTop) {
-		$("header").stop().animate({
-			"max-width":"1600px",
-			"top":"1em"
-		}, 200)
-	}
-});
+
 
 
 //메인배너 / .bans
